303549b3ea2058776fb0a855b43eb317f023ed13
Alieu-K <alieu2567@gmail.com>
2024-04-02 19:05:17 -0400

    Integrated MySQL work bench into code

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import mysql.connector
+
+connection = mysql.connector.connect(user = "root", database = "example", password = "FireCarpet657@")
+cursor = connection.cursor()
+testQuery = ("SELECT * FROM online_banking")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
+
+
 account_list = {
     "personal":5738299
 }
@@ -20,23 +34,32 @@ def introduction():
             #make_account()
             x = False
         else:
-            print("Please type yes or no in ")
+            print("Please type yes or no")
 
 def select_account():
     y = 0
     for account in account_list:
         y += 1
         print(f"{y}: {account}")
-    account_name = input("Type the name of the account you would like to use: ")
+    z = True
+    while (z):
+        account_name = input("Type the name of the account you would like to use: ")
 
-    if account_name in account_list:
-        account_password = int(input("Ok, what is the password for this account? "))
-        if account_password == account_list.get(account_name):
-            print("yay")
+        if account_name in account_list:
+            account_password = int(input("Ok, what is the password for this account? "))
+            if account_password == account_list.get(account_name):
+                print("yay")
+                z = False
+
+            else:
+                print("nay")
+                z = False
         else:
-            print("nay")
+            print("Please select from the accounts listed above.")
 
     
-
+"""
 if __name__ == "__main__":
     introduction()
+
+"""
