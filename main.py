fe5d5c5df721eadaa223c26d6bfff77149c81078
Alieu-K <alieu2567@gmail.com>
2024-04-04 21:31:23 -0400

    Able to make an account and intialize it with MySQL

--- main.py
+++ main.py
@@ -2,10 +2,25 @@ import mysql.connector
 
 connection = mysql.connector.connect(user = "root", database = "example", password = "FireCarpet657@")
 cursor = connection.cursor()
+testQuery = 'SELECT * FROM online_banking'
 
+#x = ("INSERT INTO online_banking (idOnline_Banking, account_name, password, account_email, total_amount, latest_transaction) VALUE (2, 'Personal', 'BestPassword123@','bestEmail@email.com', 100, 'N/A')")
+# cursor.execute(x)
+# connection.commit()
+cursor.reset
+
+cursor.execute(testQuery)
+
+
+for item in cursor:
+    print(item)
+    
+
+
+special_charcters = "(''!@#$%^&*()_-=+[]{\|;:/.,<>~`?}{)"
 
 def introduction():
-    print("Quick and Easy Bank")
+    print("\nQuick and Easy Bank")
     print("---------------------------------------------")
     name = input("Hello welcome to Quick and Easy Bank, what is your name? ")
     x = True
@@ -19,7 +34,7 @@ def introduction():
 
         elif account_status == "no" or account_status == "No":
             print("Ok, lets get you with an account then.")
-            #make_account()
+            make_account()
             x = False
         else:
             print("Please type yes or no")
@@ -34,28 +49,94 @@ def select_account():
         account_list.append(account[1])
 
     z = True
+    password_tries = 3
     while (z):
-        account_name = input("Type the name of the account you would like to use: ")
+        if password_tries == 0:
+            print("You have run out of password tries, you will be kicked out now.")
+            z = False
+        if(z):
+            account_name = input("Type the name of the account you would like to use: ")
+        
+            if account_name in account_list:
+                account_password = input("Ok, what is the password for this account? ")
+                cursor.execute("SELECT password FROM online_banking")
+                password = cursor.fetchone()
+                password = ''.join(password)
 
-        if account_name in account_list:
-            account_password = input("Ok, what is the password for this account? ")
-            cursor.execute("SELECT password FROM online_banking")
-            password = cursor.fetchone()
-            password = ''.join(password)
+                if account_password == password:
+                    print("yay")
+                    z = False
 
-            if account_password == password:
-                print("yay")
-                z = False
+                else:
+                    password_tries -= 1
+                    print(f"Please make sure you typed in your password correctly, you have {password_tries} left.")
 
             else:
-                print("nay")
-                z = False
-        else:
-            print("Please select from the accounts listed above.")
+                print("Please select from the accounts listed above.")
 
 def make_account():
-    print("")
+    account_name = input("What do you want the name of your account to be? No numbers or special charcters. ")
 
+    if name_has_number(account_name) == False and name_has_special_char(account_name) == True:
+    
+        password_creation_check = True
+        while(password_creation_check):
+            account_password = input("Ok, now what do you want your password to be? ")
+
+            if password_char_count(account_password):
+                starting_total = 0.0
+                
+                account_adding = (f"INSERT INTO online_banking (account_name, password, account_email, total_amount, latest_transaction) VALUE ('{account_name}', '{account_password}', '@gmail.com', { starting_total}, 'N/A')")
+                breakpoint()
+                cursor.execute(account_adding)
+                connection.commit()
+                
+                print("okay")
+                password_creation_check = False
+            else:
+                print("Please type a password that has an upper case letter, a special character, a number, and is between 8-25 characters long.")
+        
+    else:
+        print("Please don't include numbers or special charcters in the name.")
+        make_account()
+
+
+def name_has_number(input_string):
+    return any(char.isdigit() for char in input_string)
+def name_has_special_char(input_string):
+    char_check = True
+    for char in input_string:
+        if char in special_charcters:
+            char_check = False
+    return char_check
+def password_char_count(input_string):
+    char_length = 0
+    upper_count = 0
+    special_char_count = 0
+    number_count = 0
+    space_count = 0
+
+    for char in input_string:
+        char_length += 1
+        if char.isupper():
+            upper_count += 1
+        if char in special_charcters:
+            special_char_count += 1
+        if char.isdigit():
+            number_count += 1
+        if char == " ":
+            space_count = 1
+
+
+    if space_count ==  1:
+        return False
+    elif char_length >= 8 and char_length <= 25 and upper_count >= 1 and special_char_count >= 1 and number_count >= 1: 
+        return True
+    else:
+        return False
+        
+    
+            
 
 if __name__ == "__main__":
     introduction()
